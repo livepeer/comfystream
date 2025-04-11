6aee072dd75b80a840661e5eef46e1b2d9052bd2
Elite Encoder <john@eliteencoder.net>
2025-04-10 20:23:24 -0400

    fix for example.py

--- example.py
+++ example.py
@@ -1,32 +1,34 @@
+import av
 import torch
 import asyncio
 import json
+import numpy as np
 
-from comfystream.client import ComfyStreamClient
+from comfystream.utils import convert_prompt
+from comfy.cli_args_types import Configuration
+from comfy.client.embedded_comfy_client import EmbeddedComfyClient
+from comfystream import tensor_cache
 
 
 async def main():
     # Configuration options:
     # https://github.com/hiddenswitch/ComfyUI/blob/89d07f3adf32a6703181343bc732bd85104bb653/comfy/cli_args_types.py#L37
-    cwd = "/home/user/comfy-hiddenswitch"
-    client = ComfyStreamClient(cwd=cwd)
+    cwd = "/workspace/ComfyUI"
+    config = Configuration(cwd=cwd, disable_cuda_malloc=True, gpu_only=True, comfyui_inference_log_level=None)
+    
 
-    with open("./workflows/tensor-utils-example-workflow.json", "r") as f:
+    with open("./workflows/comfystream/sd15-tensorrt-api.json", "r") as f:
         prompt = json.load(f)
+    prompt = convert_prompt(prompt)
 
-    client.set_prompt(prompt)
-
-    # Comfy will cache nodes that only need to be run once (i.e. a node that loads model weights)
-    # We can run the prompt once before actual inputs come in to "warmup"
-    input = torch.randn(1, 512, 512, 3)
-    await client.queue_prompt(input)
-
-    # Now we are ready to process actual inputs
-    # We can pass an image tensor directly
-    input = torch.randn(1, 512, 512, 3)
-    output = await client.queue_prompt(input)
-    print(output.shape)
-
+    async with EmbeddedComfyClient(config) as client:
+        # Comfy will cache nodes that only need to be run once (i.e. a node that loads model weights)
+        # We can run the prompt once before actual inputs come in to "warmup"
+        input = torch.randn(1, 512, 512, 3)
+        tensor_cache.image_inputs.put(input)
+        await client.queue_prompt(prompt)
+        output = await tensor_cache.image_outputs.get()
+        print(output.shape)
 
 if __name__ == "__main__":
     asyncio.run(main())
