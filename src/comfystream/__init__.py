dc71770400552394e62a3e3185eea9dcd57a8913
John | Elite Encoder <john@eliteencoder.net>
2025-07-10 12:15:47 -0400

    improve warmup options, move workflows to server package data

--- src/comfystream/__init__.py
+++ src/comfystream/__init__.py
@@ -3,16 +3,11 @@ from .pipeline import Pipeline
 from .server.utils import temporary_log_level
 from .server.utils import FPSMeter
 from .server.metrics import MetricsManager, StreamStatsManager
-from .prompts import DEFAULT_PROMPT, DEFAULT_SD_PROMPT, INVERTED_PROMPT
-
 __all__ = [
     'ComfyStreamClient',
     'Pipeline',
     'temporary_log_level',
     'FPSMeter',
     'MetricsManager',
-    'StreamStatsManager',
-    'DEFAULT_PROMPT',
-    'DEFAULT_SD_PROMPT',
-    'INVERTED_PROMPT'
+    'StreamStatsManager'
 ]
