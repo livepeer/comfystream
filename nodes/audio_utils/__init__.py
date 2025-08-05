c822b1f2352541fb006f92e06d86ea376718f644
Jason <83615043+JJassonn69@users.noreply.github.com>
2025-08-06 01:32:06 +0300

    refactor: remove the whisper realted code to custom node (#341)
    * remove SRTGeneratorNode, AudioTranscriptionNode and related mappings; update debug_info in workflow
    * nodes: use realtime-transcription branch for ComfyUI-Stream-Pack
    ---------
    
    Co-authored-by: John | Elite Encoder <john@eliteencoder.net>

--- nodes/audio_utils/__init__.py
+++ nodes/audio_utils/__init__.py
@@ -2,14 +2,12 @@ from .load_audio_tensor import LoadAudioTensor
 from .load_audio_tensor_stream import LoadAudioTensorStream
 from .save_audio_tensor import SaveAudioTensor
 from .pitch_shift import PitchShifter
-from .audio_transcription_node import AudioTranscriptionNode
 
 NODE_CLASS_MAPPINGS = {
     "LoadAudioTensor": LoadAudioTensor, 
     "LoadAudioTensorStream": LoadAudioTensorStream,
     "SaveAudioTensor": SaveAudioTensor, 
-    "PitchShifter": PitchShifter, 
-    "AudioTranscriptionNode": AudioTranscriptionNode
+    "PitchShifter": PitchShifter
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS"]
