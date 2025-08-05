8820cb6d2eba8e36d53d5a498b9ff21e8817dd53
Jason Stone <ganesh_friend2010@yahoo.com>
2025-08-05 19:21:33 +0000

    remove AudioTranscriptionNode and related mappings; update debug_info in workflow

--- nodes/text_utils/__init__.py
+++ nodes/text_utils/__init__.py
@@ -1,10 +1,9 @@
 """Text utility nodes for ComfyStream"""
 
 from .save_text_tensor import SaveTextTensor
-from .srt_generator_node import SRTGeneratorNode
 
-NODE_CLASS_MAPPINGS = {"SaveTextTensor": SaveTextTensor, "SRTGeneratorNode": SRTGeneratorNode}
-NODE_DISPLAY_NAME_MAPPINGS = {"SaveTextTensor": "Save Text Tensor", "SRTGeneratorNode": "SRT Generator"}
+NODE_CLASS_MAPPINGS = {"SaveTextTensor": SaveTextTensor}
+NODE_DISPLAY_NAME_MAPPINGS = {"SaveTextTensor": "Save Text Tensor"}
 
 
 __all__ = ["NODE_CLASS_MAPPINGS"]
