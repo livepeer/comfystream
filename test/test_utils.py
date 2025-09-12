fa12adcd14fad988fe72a87bb007e3032dc6362c
John | Elite Encoder <john@eliteencoder.net>
2025-09-12 22:01:18 +0000

    tests: use variety of outputs for self-documentation

--- test/test_utils.py
+++ test/test_utils.py
@@ -14,7 +14,7 @@ def prompt_invalid_num_inputs():
     return {
         "1": {"inputs": {}, "class_type": "LoadImage"},
         "2": {"inputs": {}, "class_type": "LoadImage"},
-        "3": {"inputs": {}, "class_type": "LoadImage"},
+        "3": {"inputs": {}, "class_type": "LoadAudioTensor"},
     }
 
 
@@ -25,9 +25,9 @@ def prompt_invalid_num_outputs():
         "1": {"inputs": {}, "class_type": "LoadImage"},
         # four output nodes to exceed the limit of three
         "2": {"inputs": {}, "class_type": "PreviewImage"},
-        "3": {"inputs": {}, "class_type": "SaveImage"},
+        "3": {"inputs": {}, "class_type": "SaveAudioTensor"},
         "4": {"inputs": {}, "class_type": "PreviewImage"},
-        "5": {"inputs": {}, "class_type": "SaveImage"},
+        "5": {"inputs": {}, "class_type": "SaveTextTensor"},
     }
 
 
