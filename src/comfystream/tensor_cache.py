6b9d215d6b9fb537a1e662f1796e9be1d4ba0c84
lukiod <mohakgupta0981@gmail.com>
2025-09-27 03:33:28 +0000

    tested and fixed merge conflict

--- src/comfystream/tensor_cache.py
+++ src/comfystream/tensor_cache.py
@@ -7,7 +7,6 @@ from asyncio import Queue as AsyncQueue
 from typing import Union
 
 # TODO: improve eviction policy fifo might not be the best, skip alternate frames instead
-# Increased queue size to support batch processing (up to 8 images at a time)
 image_inputs: Queue[Union[torch.Tensor, np.ndarray]] = Queue(maxsize=1)
 image_outputs: AsyncQueue[Union[torch.Tensor, np.ndarray]] = AsyncQueue()
 
