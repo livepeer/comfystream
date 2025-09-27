f9db0557fc54b73e9651776f54fdb64c9fe4fc08
John | Elite Encoder <john@eliteencoder.net>
2025-09-26 22:37:08 -0400

    add entrypoint update setup_nodes (wip)

--- docker/entrypoint-byow.sh
+++ docker/entrypoint-byow.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 set -e
 
-echo "Setting up ComfyUI workspace..."
+# If an command was passed, run it instead of setting up ComfyUI workspace
+if [[ $# -ne 0 ]]; then
+  exec "$@"
+else
+  echo "Setting up ComfyUI workspace..."
+  export VIRTUAL_ENV=/workspace/ComfyUI/.venv
+  export PATH="$VIRTUAL_ENV/bin:$PATH"
+  source $VIRTUAL_ENV/bin/activate
+  comfy set-default /workspace/ComfyUI
 
-comfy set-default /workspace/ComfyUI
+  echo "Refreshing Custom Nodes..."
+  cat /workspace/ComfyUI/custom_nodes/comfystream/src/comfystream/scripts/requirements.txt
+  uv pip install -r /workspace/ComfyUI/custom_nodes/comfystream/src/comfystream/scripts/requirements.txt
+#  python custom_nodes/comfystream/src/comfystream/scripts/setup_nodes.py --workspace /workspace/ComfyUI --pull-branches
+
+  echo "ComfyUI workspace setup complete!"
+  # Set up completion if not already done
+  if [[ ! -f ~/.local/share/bash-completion/completions/comfy ]]; then
+      comfy --install-completion 2>/dev/null || true
+  fi
+
+  echo "Starting ComfyUI server..."
+  exec comfy -- launch -- --listen 0.0.0.0 --port 8188 --front-end-version Comfy-Org/ComfyUI_frontend@v1.24.2
+fi
+
 # # Check if /workspace/ComfyUI is a symlink (indicating mounted volume)
 # if [[ -L "/workspace/ComfyUI" ]]; then
 #     echo "Detected mounted workspace (symlink found)"
@@ -71,18 +93,6 @@ comfy set-default /workspace/ComfyUI
 #     source .venv/bin/activate
 # fi
 
-# Set up completion if not already done
-if [[ ! -f ~/.local/share/bash-completion/completions/comfy ]]; then
-    comfy --install-completion 2>/dev/null || true
-fi
-
-echo "ComfyUI workspace setup complete!"
-
-
-# Execute the command passed to docker run, or start ComfyUI by default
-if [[ $# -eq 0 ]]; then
-    echo "Starting ComfyUI server..."
-    exec comfy launch -- --listen 0.0.0.0 --port 8188 --front-end-version Comfy-Org/ComfyUI_frontend@v1.24.2
 else
     exec "$@"
 fi
